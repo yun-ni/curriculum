58827ecefb1d76354df702458ba020c9d905849a
KUU N <kuun@KUUnoMacBook-Air.local>
2026-04-15 20:54:16 +0900

    [ 20260415- マイグレーションファイル作成 - 実装中 ]

---
+++ app/database/migrations/2026_04_15_111913_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('email', 225)->unique();
+            $table->string('password', 225);
+            $table->tinyInteger('del_flg')
+                  ->default(0)
+                  ->comment('0=有効, 1=削除');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
