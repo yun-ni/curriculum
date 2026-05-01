0ffff2d02de6d05a444c98145d5685dbafa2102b
KUU N <kuun@KUUnoMacBook-Air.local>
2026-05-01 23:27:51 +0900

    [ 20260501- View作成（ペットの一覧画面） ]

--- app/database/seeds/PetSeeder.php
+++ app/database/seeds/PetSeeder.php
@@ -17,13 +17,33 @@ class PetSeeder extends Seeder
         $user = User::first(); // Usersテーブルから最初の1件を取得
 
         $params = [
-            'name' => 'くうさん',
-            'birth_date' => '2021-06-25',
-            'gender' => 1, // TINYINT型
-            'profile_image' => 'images/kuusan.png',
-            'user_id' => $user->id,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
+            [
+                'name' => 'くうさん',
+                'birth_date' => '2021-06-25',
+                'gender' => 1, // TINYINT型
+                'profile_image' => 'images/kuusan.png',
+                'user_id' => $user->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'ぱぐぞう',
+                'birth_date' => '2022-05-20',
+                'gender' => 1, // TINYINT型
+                'profile_image' => 'images/ぱぐぞう.png',
+                'user_id' => $user->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'モフリシャス',
+                'birth_date' => '2023-01-01',
+                'gender' => 1, // TINYINT型
+                'profile_image' => 'images/モフリシャス.png',
+                'user_id' => $user->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
         ];
 
         DB::table('pets')->insert($params);
--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -18,6 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Bootstrap Javascript(jQuery含む) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="">
@@ -28,24 +36,34 @@
         </nav>
         <main>
             <div class = "container">
-                <div class = "">
-                    <div class = "">
-                        <tbody>
-                            @foreach ($pets as $pet)
-                                <tr>
-                                    <a href="">
+                {{-- row-cols-* で1行あたりの列数を指定 (例: md以上で3列、smで2列、xsで1列) --}}
+                <div class="row">
+                    @foreach ($pets as $pet)
+                        <div class = "col-md-4 mb-4">
+                            <div class = "p-3 border bg-light h-100 d-flex flex-column align-items-center justify-content-center" 
+                                 style="aspect-ratio: 1 / 1;">
+                                <div class="text-center"> {{-- 画像も中央寄せ --}}
+                                        <a href="">
                                         <img src="{{ asset($pet['profile_image']) }}" 
-                                            alt="プロフィール画像" 
-                                            width="200" height="200" 
-                                            style="border-radius: 50%; object-fit: cover;">
-                                        <th>{{ $pet['name'] }}</th>
+                                        alt="プロフィール画像" 
+                                        width="200" height="200" 
+                                        style="border-radius: 50%; object-fit: cover;">
                                     </a>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </div>
-                    <div class = "col">
-                        <a href="" >＋ペットを追加</a>
+                                </div>
+                                <div class="text-center">
+                                    <p>{{ $pet['name'] }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+                    <div class = "col-md-4 mb-4">
+                        <div class = "p-3 border bg-light h-100 d-flex flex-column align-items-center justify-content-center" 
+                             style="aspect-ratio: 1 / 1;">
+                        <a href="" class="text-decoration-none text-center">
+                            <p class="mb-0">＋ペットを追加</p>
+                        </a>
+                        </div>
                     </div>
                 </div>
             </div>
