7c605990448a4ad8111e1dacd5544722c125631c
KUU N <kuun@KUUnoMacBook-Air.local>
2026-06-07 19:42:22 +0900

    [ 20260607- 非同期処理/無限スクロール -実装]

--- app/app/Http/Controllers/DisplayController.php
+++ app/app/Http/Controllers/DisplayController.php
@@ -2,6 +2,8 @@
 // DBからのデータ取得の管理用
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 use App\User;
 use App\Pet;
 use App\Health;
@@ -26,7 +28,7 @@ class DisplayController extends Controller
         ]);
     }
 
-    public function petIndex(int $petId) {
+    public function petIndex(Request $request, int $petId) {
         $pet = Pet::findOrFail($petId);
         $healths = Health::where('pet_id', $petId)->get();
         $visits = Visit::where('pet_id', $petId)->get();
@@ -39,6 +41,21 @@ class DisplayController extends Controller
 
         if (is_null($todayHealth)) {
             return redirect()->route('create.health_form', ['id' => $petId]);
+        }
+
+        // 非同期処理 記録を無限スクロール
+        // レコードを10件ずつ取得
+        $health_records = Health::where('pet_id', $petId)->latest()->paginate(10);
+        $visit_records = Visit::where('pet_id', $petId)->latest()->paginate(10);
+
+        // Ajax通信の場合はJSONを返す
+        if ($request->ajax()) {
+            return response()->json([
+                'health_html' => view('pets.health_list', compact('health_records'))->render(),
+                'visit_html' => view('pets.visit_list', compact('visit_records'))->render(),
+                'healthHasMorePages' => $health_records->hasMorePages(),
+                'visitHasMorePages' => $visit_records->hasMorePages(),
+            ]);
         }
 
         return view('pets.index', compact('pet', 'healths', 'visits'));
